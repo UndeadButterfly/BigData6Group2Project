b9ea2dec386e3e409cfeb33032f88be50815cbeb
oyunmintrio95 <oyunmintrio95@gmail.com>
2022-12-11 22:05:20 +0900

    layout 구성

--- Acorn_Project1/planner/locb.js
+++ Acorn_Project1/planner/locb.js
@@ -1,22 +1,45 @@
-
-
-
-function myFunction(){
-    const recommendBtn = document.querySelector("#hiddenBox");
-    // recommendBtn.classList.toggle("foldableBoxRight")
-    if(recommendBtn.style.display == "none"){
-        recommendBtn.style.display = "block";
-        window.setTimeout(function(){
-            recommendBtn.style.opacity=1;
-            // recommendBtn.style.transform='scale(1)';
-
-        },0);
-    }else{
-        recommendBtn.style.opacity=0;
-        // recommendBtn.style.transform = 'scale(0)';
-        window.setTimeout(function(){
-            recommendBtn.style.display="none";
-        },500);
-        
-    }
+function recommendToggle(){
+    const hiddenBox = document.querySelector("#hiddenBox");
+    hiddenBox.classList.toggle("d-none");
 }
+const recLocCardList = document.getElementById("recLocCardList");
+const recFoodCardList = document.getElementById("recFoodCardList");
+const recSleepCardList=document.getElementById("recSleepCardList");
+const tourBtn = document.getElementById("tourBtn");
+const foodBtn = document.getElementById("foodBtn");
+const sleepBtn = document.getElementById("sleepBtn");
+
+console.log(recFoodCardList);
+console.log(recSleepCardList);
+console.log(recLocCardList);
+
+document.getElementById("tourBtn").onclick=()=>{
+    tourBtn.classList.add("active");
+    foodBtn.classList.remove("active");
+    sleepBtn.classList.remove("active");
+
+    recLocCardList.classList.remove("d-none");
+    recFoodCardList.classList.add("d-none");
+    recSleepCardList.classList.add("d-none");
+};
+
+document.getElementById("foodBtn").onclick=()=>{
+    tourBtn.classList.remove("active");
+    foodBtn.classList.add("active");
+    sleepBtn.classList.remove("active");
+
+    recLocCardList.classList.add("d-none");
+    recFoodCardList.classList.remove("d-none");
+    recSleepCardList.classList.add("d-none");
+}
+
+document.getElementById("sleepBtn").onclick=()=>{
+    tourBtn.classList.remove("active");
+    foodBtn.classList.remove("active");
+    sleepBtn.classList.add("active");
+
+    recLocCardList.classList.add("d-none");
+    recFoodCardList.classList.add("d-none");
+    recSleepCardList.classList.remove("d-none");
+}
+
