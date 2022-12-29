a2ee6dc7f826eb324af9167e54c7d3baa27c8e25
HYOBEAN <wjd4427947@naver.com>
2022-12-29 09:51:36 +0900

    project

--- tripwithyou_spring/src/main/java/com/twy/tripwithyou_spring/service/CoursePlaceService.java
+++
@@ -1,9 +0,0 @@
-package com.twy.tripwithyou_spring.service;
-
-import com.twy.tripwithyou_spring.dto.CoursePlaceDto;
-
-import java.util.List;
-
-public interface CoursePlaceService {
-    List<CoursePlaceDto> list(int courseNo);
-}
