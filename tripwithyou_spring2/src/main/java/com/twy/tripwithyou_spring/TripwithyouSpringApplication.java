c92ef8428fc6cad05e3c8f6cc360ec96b2bc3ae7
oyunmintrio95 <oyunmintrio95@gmail.com>
2022-12-29 16:04:02 +0900

    commit

--- tripwithyou_spring2/src/main/java/com/twy/tripwithyou_spring/TripwithyouSpringApplication.java
+++ tripwithyou_spring2/src/main/java/com/twy/tripwithyou_spring/TripwithyouSpringApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TripwithyouSpringApplication {
+public class  TripwithyouSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TripwithyouSpringApplication.class, args);
